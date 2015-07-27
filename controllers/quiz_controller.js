272dc3535f83ad421c741958a5ff46c29072374a
Miguel Sanchez <sob444@gmail.com>
2015-07-27 19:16:27 +0200

    Despliegue DB en Heroku

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -1,15 +1,42 @@
-// GET /quizes/question
-exports.question = function(req, res) {
-   res.render('quizes/question', {pregunta: 'Capital de Italia'});
+var models = require('../models/models.js');
+
+// Autoload - factoriza el código si ruta incluye :quizId
+
+exports.load = function(req, res, next, quizId) {
+   models.Quiz.find(quizId).then(
+     function(quiz) {
+       if (quiz) {
+         req.quiz = quiz;
+         next();
+       } else { next(new Error('No existe quizId=' + quizId));}
+     }
+    ).catch(function(error) { next(error);});
+  };
+
+// GET /quizes/:id
+exports.show = function(req, res) {
+     res.render('quizes/show', {quiz: req.quiz})
 };
 
-// GET /quizes/answer
+// GET /
+
+
+// GET /quizes
+exports.index = function(req, res) {
+   models.Quiz.findAll().then(
+     function(quizes) {
+       res.render('quizes/index.ejs', {quizes: quizes});
+     }
+   ).catch (function (error) { next (error);})
+};
+
+// GET /quizes/:id/answer
 exports.answer = function(req, res) {
-   if (req.query.respuesta === 'Roma'){
-      res.render('quizes/answer', {respuesta: 'Correcto'});
-   } else {
-      res.render('quizes/answer', {respuesta: 'Incorrecto'});
+  var resultado ='Incorrecto'; 
+  if (req.query.respuesta === req.quiz.respuesta){
+      resultado= 'Correcto';
    }
+   res.render('quizes/answer',{quiz:req.quiz, respuesta: resultado});
 };
 
 // GET /quizes/author
--- routes/index.js
+++ routes/index.js
@@ -5,11 +5,15 @@ var quizController = require('../controllers/quiz_controller');
 
 /* GET home page. */
 router.get('/', function(req, res) {
-  res.render('index', { title: 'Quiz' });
+res.render('index', { title: 'Quiz' });
 });
 
-router.get('/quizes/question', quizController.question);
-router.get('/quizes/answer', quizController.answer);
-router.get('/quizes/author', quizController.author);
+// Autoload de comandos con :quizId
+router.param('quizId', quizController.load); // autoload :quizId
+
+router.get('/quizes',                      quizController.index);
+router.get('/quizes/:quizId(\\d+)',        quizController.show);
+router.get('/quizes/:quizId(\\d+)/answer', quizController.answer);
+router.get('/quizes/author',               quizController.author);
 
 module.exports = router;
