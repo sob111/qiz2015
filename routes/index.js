8f496486c85b868362951810fb81b3d25f22161d
Miguel Sanchez <sob444@gmail.com>
2015-08-03 18:29:53 +0200

    Busquedas

--- routes/index.js
+++ routes/index.js
@@ -11,9 +11,12 @@ res.render('index', { title: 'Quiz' });
 // Autoload de comandos con :quizId
 router.param('quizId', quizController.load); // autoload :quizId
 
-router.get('/quizes',                      quizController.index);
-router.get('/quizes/:quizId(\\d+)',        quizController.show);
-router.get('/quizes/:quizId(\\d+)/answer', quizController.answer);
-router.get('/quizes/author',               quizController.author);
+//router.get('/quizes(\\?busqueda=:quizPregunta', quizController.busca);
+router.get('/quizes/all',                    quizController.index);
+router.get('/quizes/:quizId(\\d+)',          quizController.show);
+router.get('/quizes/:quizId(\\d+)/answer',   quizController.answer);
+router.get('/quizes/author',                 quizController.author);
+router.get('/quizes/search',                 quizController.busca);
+router.get('/quizes/result',                 quizController.result);
 
 module.exports = router;
